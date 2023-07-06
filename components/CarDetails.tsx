b9a1c8893bcdda8d089bdb0f7cca41b88c4f9c0a
Boris Klco <klco@pm.me>
2023-07-06 08:47:50 +0000

    fixing Modal

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -32,11 +32,11 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
                 leaveTo="opacity-0 scale-95"
               >
                 <Dialog.Panel
-                  className="relative w-full max-w-lg max-h-[90vh] overflow-y-auto transform rounded-2xl bg-white
-                text-left shadow-xsl transition-all flex flex-col gap-5"
+                  className="relative w-full max-w-lg max-h-[90vh] overflow-hidden transform rounded-2xl bg-white
+                p-6 text-left shadow-xl transition-all flex flex-col gap-5"
                 >
                   <button
-                    className="absolute top-2 right-2 z-10 w-fit p-2 bg-primary-blue rounded-full"
+                    className="absolute top-2 right-2 z-10 w-fit p-2 bg-primary-blue-100 rounded-full"
                     type="button"
                     onClick={closeModal}
                   >
@@ -78,6 +78,21 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
                       {car.make} {car.model}
                     </h2>
 
+                    <div className="mt-3 flex flex-wrap gap-4">
+                      {Object.entries(car).map(([key, value]) => (
+                        <div
+                          className="flex justify-between gap-5 w-full text-right"
+                          key={key}
+                        >
+                          <h4 className="text-gray capitalize">
+                            {key.split("_").join(" ")}
+                          </h4>
+                          <p className="text-black-100 font-semibold">
+                            {value}
+                          </p>
+                        </div>
+                      ))}
+                    </div>
                   </div>
                 </Dialog.Panel>
               </Transition.Child>
